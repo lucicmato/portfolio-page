7d9bc30352e3c1fe02fdd911a0c95bff009dd983
lucicmato <lucicmato93@gmail.com>
2025-02-15 15:23:02 +0100

    Project flex space fix.

--- src/components/Content/Projects/ProjectCard.tsx
+++ src/components/Content/Projects/ProjectCard.tsx
@@ -5,7 +5,13 @@ interface ProjectCardProps {
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ children }) => {
-  return <div className={`pt-12 w-full z-10`}>{children}</div>;
+  return (
+    <div
+      className={`pt-12 w-full z-10 md:flex md:flex-col md:justify-between md:h-full xl:flex xl:flex-col xl:justify-between xl:h-full`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default ProjectCard;
